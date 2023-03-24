e3b63164fc33ff08421da5da148235c3201e8961
Heather J <37090338+HeatherJia@users.noreply.github.com>
2023-03-23 22:59:44 -0500

    Update applyHomography.m

--- applyHomography.m
+++ applyHomography.m
@@ -1,9 +1,11 @@
 function dest_pts_nx2 = applyHomography(H_3x3, src_pts_nx2)
-% This function transforms source image points
-n = size(src_pts_nx2, 1);
-dest_pts_nx2 = zeros(n, 2);
-
-for i = 1 : n
-    p = H_3x3*[src_pts_nx2(i, :), 1]';
-    dest_pts_nx2(i, :) = [round(p(1) / p(3)), round(p(2) / p(3))];
+% This function transforms the source image
+dimsize = size(src_pts_nx2, 1);
+dest_pts_nx2 = zeros(dimsize, 2);
+for i = 1 : dimsize
+    a = src_pts_nx2(i, :);
+    s = [a, 1];
+    d = H_3x3 * s';
+    x = d(1) / d(3); y = d(2) / d(3);
+    dest_pts_nx2(i, :) = [round(x), round(y)];
 end
