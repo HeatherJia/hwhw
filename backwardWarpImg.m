7166c8766efaad3b2f6bd67dd14168da025c386a
Heather J <37090338+HeatherJia@users.noreply.github.com>
2023-03-23 23:11:05 -0500

    Update backwardWarpImg.m

--- backwardWarpImg.m
+++ backwardWarpImg.m
@@ -1,21 +1,20 @@
 function [mask, result_img] = backwardWarpImg(src_img, resultToSrc_H,...
     dest_canvas_width_height)
 % This function warps an image based on a homography
-width = dest_canvas_width_height(1);
-height = dest_canvas_width_height(2);
-x_bnd = size(src_img, 2);
-y_bnd = size(src_img, 1);
+w = dest_canvas_width_height(1);
+h = dest_canvas_width_height(2);
 
 % create mask and result tmp
-mask = zeros(height, width);
-result_img = zeros(height, width, 3);
+mask = zeros(h, w);
+result_img = zeros(h, w, 3);
 
-for i = 1 : width
-    for j = 1 : height
-        p = resultToSrc_H * [j; i; 1];
-        x = round(p(1) / p(3)); y = round(p(2) / p(3));
-        if (x >= 1 && x <= x_bnd && y >= 1 && y <= y_bnd)
-            result_img(i, j, :) = src_img(y, x, :); mask(i, j) = 1;
+for i = 1 : h
+    for j = 1 : w
+        d = resultToSrc_H * [j; i; 1];
+        x = round(d(1) / d(3)); y = round(d(2) / d(3));
+        if (x >= 1 && x <= size(src_img, 2) && y >= 1 && y <= size(src_img, 1))
+            result_img(i, j, :) = src_img(y, x, :);
+            mask(i, j) = 1;
         end
     end
 end
