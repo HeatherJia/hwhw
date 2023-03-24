59562b370ab8a1e796792f8382e95d665c1d6aa2
Heather J <37090338+HeatherJia@users.noreply.github.com>
2023-03-23 23:35:01 -0500

    Update stitchImg.m

--- stitchImg.m
+++ stitchImg.m
@@ -1,63 +1,50 @@
 function stitched_img = stitchImg(varargin)
-% Stitch input images into one mosaic
+% This function stitches the input images into one mosaic
+ransac_n = 50; ransac_eps = 5;
+img = varargin{1};
+ori_h = size(img, 1); ori_w = size(img, 2);
+mask1 = ones(ori_h, ori_w);
 
-% get the first image
-cur = varargin{1};
-height = size(cur, 1);
-width = size(cur, 2);
-cur_mask = ones(height, width);
-
-% iterate through the images
 for i = 2 : nargin
-    % get the homography (with RANSAC)
-    [height, width, channel] = size(cur);
-    impl = 'MATLAB';
-    [xs, xd] = genSIFTMatches(varargin{i}, cur, impl);
-    ransac_n = 100;
-    ransac_eps = 10;
-    [inliers_id, H] = runRANSAC(xs, xd, ransac_n, ransac_eps);
-
-    h = size(varargin{i}, 1);
-    w = size(varargin{i}, 2);
+    [ori_h, ori_w, c] = size(img);
+    [xs, xd] = genSIFTMatches(varargin{i}, img, "MATLAB");
+    [tmp, H] = runRANSAC(xs, xd, ransac_n, ransac_eps);
+    h = size(varargin{i}, 1); w = size(varargin{i}, 2);
     orig = [1, 1; w, 1; 1, h; w, h];
     dest = applyHomography(H, orig);
-    
-    % define the boundaries
-    left = min(dest(:, 1));
-    right = max(dest(:, 2));
-    top = min(dest(:, 2));
-    bottom = max(dest(:, 2));
+
+    left = min(dest(:, 1)); right = max(dest(:, 1));
+    top = min(dest(:, 2)); bottom = max(dest(:, 2));
 
     if left < 1
-        cur = [zeros(height, round(1-left), channel), cur];
-        cur_mask = [zeros(height, round(1-left)), cur_mask];
-        dest(:, 1) = dest(:, 1) + round(1-left);
-    end 
+        tmp_zeros1 = zeros(ori_h, round(1 - left), c);
+        img = [tmp_zeros1, img];
+        tmp_zeros2 = zeros(ori_h, round(1 - left));
+        mask1 = [tmp_zeros2, mask1];
+        dest(:, 1) = dest(:, 1) + round(1 - left);
+    end
+    if right > ori_w
+        tmp_zeros1 = zeros(ori_h, round(right - ori_w), c);
+        img = [img, tmp_zeros1];
+        tmp_zeros2 = zeros(ori_h, round(right - ori_w));
+        mask1 = [mask1, tmp_zeros2];
+    end
 
-    if right > width
-        cur = [cur, zeros(height, round(right - width), channel)];
-        cur_mask = [cur_mask, zeros(height, round(right-width))];
-    end 
-    
-    width = size(cur, 2); % update width
+    width2 = size(img, 2);
     if top < 1
-        cur = [zeros(round(1-top), width, channel); cur];
-        cur_mask = [zeros(round(1-top), width); cur_mask];
-        dest(:, 2) = dest(:, 2) + round(1-top);
-    end 
+        tmp_zeros1 = zeros(round(1 - top), width2, c);
+        img = [tmp_zeros1; img];
+        mask1 = [zeros(round(1 - top), width2); mask1];
+        dest(:, 2) = dest(:, 2) + round(1 - top);
+    end
+    if bottom > ori_h
+        tmp_zeros1 = zeros(round(bottom - ori_h), width2, c);
+        img = [img; tmp_zeros1];
+        mask1 = [mask1; zeros(round(bottom - ori_h), width2)];
+    end
 
-    if bottom > height
-        cur = [cur; zeros(round(bottom-height), width, channel)];
-        cur_mask = [cur_mask; zeros(round(bottom-height), width)];
-    end 
-
-    height = size(cur, 1); % update height
-    dest_canvas_width_height = [width, height];
-    HH = computeHomography(orig, dest);
-    [mask, dest_img] = backwardWarpImg(varargin{i}, inv(HH), dest_canvas_width_height);
-    cur = blendImagePair(uint8(dest_img*255), mask, uint8(cur*255), cur_mask, 'blend');
-    cur = double(cur) / 255;
-    cur_mask = double(mask > 0 | cur_mask > 0);
+    [mask, nimg] = backwardWarpImg(varargin{i}, inv(computeHomography(orig, dest)), [width2, size(img, 1)]);
+    img = blendImagePair(uint8(nimg * 255), mask, uint8(img * 255), mask1, 'blend');
+    img = double(img) / 255; mask1 = double(mask > 0 | mask1 > 0);
 end
-
-stitched_img = cur;
+stitched_img = img;
